4bacd17aed3363058b99cf8398edb1552094618b
Hieu Le Trung <warmdevofficial@gmail.com>
2025-12-25 09:25:15 +0700

    (Day 1) Prob 242: 10.89% Solution

--- Easy/242-ValidAnagram/Solution.java
+++ Easy/242-ValidAnagram/Solution.java
@@ -1,5 +1,24 @@
+import java.util.Arrays;
+
 class Solution {
   public boolean isAnagram(String s, String t) {
+    char[] sA = s.toCharArray();
+    char[] tA = t.toCharArray();
 
+    if (tA.length != sA.length) {
+      return false;
+    }
+
+    Arrays.sort(sA);
+    Arrays.sort(tA);
+
+    s = new String(sA);
+    t = new String(tA);
+
+    if (s.equals(t)) {
+      return true;
+    }
+
+    return false;
   }
 }
